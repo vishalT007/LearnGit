3daea34aab1590bafd1e3226dbc3d75df8e71904
vishalT007 <127886955+vishalT007@users.noreply.github.com>
2024-04-05 12:41:58 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+print("Hello")
+
+def my_function():
+    print("Hello world!!")
+
+my_function()
+
+
+def print_function():
+    print("print function")
+
+def test():
+    
+    return sum = 2+3 
+
+test()
+
+def third_function(fname):
+    print(fname + "Engineer")
+third_function("RAJESH")
